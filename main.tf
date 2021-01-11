33e640521cdf3fc4577e1cee357edf6853c71b26
Naresh Kumar Bulusu <naresh.bulusu@gmail.com>
2021-01-11 15:50:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "resource_group" {
   source  = "app.terraform.io/narulu/resource_group/azurerm"
-  version = "0.1.5"
+  version = "0.1.6"
   
   resource_group_name = var.resource_group_name
   azure_region = var.azure_region
