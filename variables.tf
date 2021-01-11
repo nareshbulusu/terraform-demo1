526789cae9ce37e6c79ce206aab16f7520f4ebdc
Naresh Kumar Bulusu <naresh.bulusu@gmail.com>
2021-01-11 16:13:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
 variable "resource_group_name" {}
 variable "azure_region" {}
 variable "tags" {}
+
+variable "storageaccountname" {}
+variable "resource_group_name" {}
+variable "location" {}
+variable "account_tier" {}
+variable "account_replication_type" {}
