a1e9aadafc538650728842cab87f6886b1eda459
Naresh Kumar Bulusu <naresh.bulusu@gmail.com>
2021-01-11 17:20:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "resource_group" {
   source                    = "app.terraform.io/narulu/resource_group/azurerm"
-  version                   = "0.1.6"
+  version                   = "0.1.7"
   
   resource_group_name       = var.resource_group_name
   azure_region              = var.azure_region
@@ -14,7 +14,7 @@ module "storage_account" {
 
   storageaccountname        = var.storageaccountname
   resource_group_name       = module.resource_group.resource_group_name
-  location                  = module.resource_group.location
+  location                  = module.resource_group.resource_group_location
   account_tier              = var.account_tier
   account_replication_type  = var.account_replication_type
   tags                      = var.tags
