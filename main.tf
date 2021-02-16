7061ad204ea4df13195e99140e22ca0a074c6e2d
Naresh Kumar Bulusu <naresh.bulusu@gmail.com>
2021-02-16 19:59:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,14 +8,3 @@ module "resource_group" {
 }
 
   
-module "storage_account" {
-  source                    = "app.terraform.io/narulu/storage_account/azurerm"
-  version                   = "0.1.2"
-
-  storageaccountname        = var.storageaccountname
-  resource_group_name       = module.resource_group.resource_group_name
-  location                  = module.resource_group.resource_group_location
-  account_tier              = var.account_tier
-  account_replication_type  = var.account_replication_type
-  tags                      = var.tags
-}
